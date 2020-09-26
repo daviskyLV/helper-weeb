995ca54fa3fb81345e88b00c5b8a7dc629a574f0
davisky <ledaviskylv@gmail.com>
2020-09-26 21:24:51 +0300

    updating code

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 const PWInfo = require("../BotSecrets/PWInfo_HelpfulWeeb.js");
 const fs = require('fs');
 const Settings = require("./Settings.js");
+const mysqlCache = require("./mysql_cache.js");
 
 const CmdsFolder = "./Commands/";
 const ListenerFolder = "./ListenerCmds/";
@@ -55,10 +56,18 @@ var AdminCmds = {
     console.log("BotInfo: ",BotInfo);
     msg.channel.send("Shutting down the bot! ;-;")
       .catch(console.log);
-
-    setTimeout((function() {
-      return process.exit();
-    }), 2500);
+    mysqlCache.ForceSave()
+    .then(function (resp){
+      setTimeout((function() {
+        return process.exit();
+      }), 2500);
+    })
+    .catch(function(resp) {
+      console.log("Error while saving!");
+      setTimeout((function() {
+        return process.exit();
+      }), 5000);
+    });
   },
   "restart": function(msg) {
     Frozen = true;
@@ -89,7 +98,7 @@ var AdminCmds = {
       msg.channel.send("Bot restarted!")
         .catch(console.log);
       return true;
-    }), 15000);
+    }), 10000);
   }
 };
 
@@ -101,13 +110,16 @@ client.on('ready', () => {
   BotInfo["JoinDate"] = client.user.createdAt.toUTCString();
   BotInfo["AvatarURL"] = client.user.displayAvatarURL();
   BotInfo["ServerCount"] = client.guilds.cache.size;
-  BotInfo["ServingAmount"] = 0;
+  BotInfo["Serving"] = {}; // dictionary of user ids tht bot is serving
   client.guilds.cache.forEach(serv => {
-    BotInfo["ServingAmount"] = BotInfo["ServingAmount"]+serv.members.cache.filter(member => member.user.bot === false).size;
+    let membs = serv.members.cache.filter(member => member.user.bot === false);
+    membs.forEach((value,key)=>{
+      BotInfo["Serving"][key] = true;
+    });
   });
 
   async function PresenceLoop() {
-    var cur = 0;
+    let cur = 0;
     while (true) {
       client.user.setPresence({ activity: { name: Settings.Presences[cur], type: 0 }, status: 'online' })
         .catch(console.log);
@@ -122,9 +134,12 @@ client.on('ready', () => {
     while (true) {
       await Waiter(Settings.StatsUpdateT);
       BotInfo["ServerCount"] = client.guilds.cache.size;
-      BotInfo["ServingAmount"] = 0;
+      BotInfo["Serving"] = {};
       client.guilds.cache.forEach(serv => {
-        BotInfo["ServingAmount"] = BotInfo["ServingAmount"]+serv.members.cache.filter(member => member.user.bot === false).size;
+        let membs = serv.members.cache.filter(member => member.user.bot === false);
+        membs.forEach((value,key)=>{
+          BotInfo["Serving"][key] = true;
+        });
       });
     }
   }
@@ -136,12 +151,12 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (!msg.author.bot && !Frozen) {
     if (msg.content.substring(0,Settings.AdminPrefix.length) == Settings.AdminPrefix && Settings.AdminId[msg.author.id.toString()]) { // admin command with prefix
-      var cmd = (msg.content.split(/\s/g)[0]).substring(Settings.AdminPrefix.length,msg.content.length).toLowerCase(); // the given command trimmed down to first whitespace
+      let cmd = (msg.content.split(/\s/g)[0]).substring(Settings.AdminPrefix.length,msg.content.length).toLowerCase(); // the given command trimmed down to first whitespace
       if (AdminCmds[cmd]) {
         AdminCmds[cmd](msg);
       }
     } else if (msg.content.substring(0,Settings.Prefix.length) == Settings.Prefix) { // basic command with prefix
-      var cmd = (msg.content.split(/\s/g)[0]).substring(Settings.Prefix.length,msg.content.length).toLowerCase(); // the given command trimmed down to first whitespace
+      let cmd = (msg.content.split(/\s/g)[0]).substring(Settings.Prefix.length,msg.content.length).toLowerCase(); // the given command trimmed down to first whitespace
       if (commandList[cmd]) {
         commandList[cmd].command(msg,client,BotInfo,{"KewlSongs":KewlSongs});
       }
@@ -158,7 +173,7 @@ client.login(PWInfo.Token);
 
 /// LOOPS ///
 async function YoutubeAPILoop() {
-    function YoutubeGET(url) {
+    let YoutubeGET = function(url) {
         request(url, {json: true}, (err,res,body) => {
             if (err) {return console.log(err);}
             if (body.items) {
